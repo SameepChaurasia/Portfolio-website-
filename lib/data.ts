21d0142cdd29f8219343d559ec9b40811c9e9419
SameepChaurasia <chaurasiasameep26@gmail.com>
2024-03-21 13:29:59 +0530

    updated data

--- lib/data.ts
+++ lib/data.ts
@@ -37,17 +37,9 @@ export const experiencesData = [
     title: "Currently Building projects on my own and contributing to open source",
     location: "India",
     description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+      "Working on my own by learning new technologic and building projects and contributing to open source etc.",
     icon: React.createElement(CgWorkAlt),
     date: "2019 - 2021",
-  },
-  {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
-    description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-    icon: React.createElement(FaReact),
-    date: "2021 - present",
   },
 ] as const;
 
